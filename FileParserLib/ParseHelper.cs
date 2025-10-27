b66bb3a4ba7908c8b5a3e20e6c0549d267a065d2
Amanda Olving <amanda.olving@gmail.com>
2025-10-27 12:00:11 +0100

    Add logic for parsing json according to class (props)

--- FileParserLib/ParseHelper.cs
+++ FileParserLib/ParseHelper.cs
@@ -1,4 +1,5 @@
 using System.Text.Json;
+using System.Text.Json.Nodes;
 
 public static class ParseHelper
 {
@@ -6,5 +7,34 @@ public static class ParseHelper
     {
         using var doc = JsonDocument.Parse(rawData);
         return FlattenJsonElement(doc.RootElement);
+    }
+
+    public static IEnumerable<string[]> FlattenJsonElement(JsonElement element)
+    {
+        if (element.ValueKind == JsonValueKind.Object)
+        {
+            var objValues = new List<string>();
+
+            foreach (var prop in element.EnumerateObject())
+            {
+                objValues.Add($"{prop.Name}: {prop.Value}");
+            }
+
+            yield return objValues.ToArray();
+            yield break;
+        }
+
+        if (element.ValueKind == JsonValueKind.Array)
+        {
+            foreach (var item in element.EnumerateArray())
+            {
+                foreach (var sub in FlattenJsonElement(item))
+                    yield return sub;
+            }
+            
+            yield break;
+        }
+
+        yield return new[] { element.ToString() };
     }
 }
