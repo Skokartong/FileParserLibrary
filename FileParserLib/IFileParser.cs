d1a5ad2848ff863c0e342015b9f184cd887a331e
Amanda Olving <amanda.olving@gmail.com>
2025-10-27 10:07:00 +0100

    Add classes that implements interfaces for file reading and parsing

--- FileParserLib/IFileParser.cs
+++ FileParserLib/IFileParser.cs
@@ -1,6 +1,8 @@
 ﻿namespace FileParserLib;
-// SUMMARY:
+// <summary>
 // This parser structures the data into more meaningful content and interprets it
+// </summary>
 public interface IFileParser
 {
+    IEnumerable<string[]> ParseContent(string fileContent);
 }
--- FileParserLib/IFileReader.cs
+++ FileParserLib/IFileReader.cs
@@ -1,7 +1,8 @@
 namespace FileParserLib;
-// SUMMARY: 
+// <summary>
 // This FileReader reads in the content from the file without interpreting it
 // It returns the raw data from the file it reads from
+// </summary>
 public interface IFileReader
 {
     Task<string> ReadAllAsync(string path);
