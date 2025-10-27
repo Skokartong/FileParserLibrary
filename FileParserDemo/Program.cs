649d61f60bf1cc6a8149ea0447d1af4022b17bf5
Amanda Olving <amanda.olving@gmail.com>
2025-10-27 14:36:20 +0100

    Add example data in folder for parsing

--- FileParserDemo/Program.cs
+++ FileParserDemo/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+using FileParserLib; 
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            var parser = new FileParser();
+
+            Console.WriteLine("Write filename here (e.g 'file.json'): ");
+            var fileName = Console.ReadLine();
+            
+            var fullPath = Path.Combine("Data", fileName!);
+            var parsedContent = await parser.ParseContentAsync(fullPath);
+
+            Console.WriteLine($"\n--- Parsed content from {fileName} ---\n");
+
+            foreach (var row in parsedContent)
+            {
+                Console.WriteLine(string.Join(", ", row));
+            }
+
+            Console.WriteLine("\n✅ Parsing done!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"❌ An error occured: {ex.Message}");
+        }
+    }
+}
